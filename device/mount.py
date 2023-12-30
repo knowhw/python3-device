8724796b6ac4610fd9d51e50b60f1669ebc11e99
knowhw <142631271+knowhw@users.noreply.github.com>
2023-12-30 15:03:06 +0000

    Update mount.py

--- device/mount.py
+++ device/mount.py
@@ -4,24 +4,35 @@ from os import *
 from device import module, state
 
 
+
 class test:
+	__result=module.collections.array([])
 	
 	@classmethod
 	def device(self):
 		return state.devices()
-		
 	def all(devices):
 		
 		
 		keys = module.collections.array(devices.keys())
+		push=__class__.__result.append
 		
 		for item in keys:
 			
-			if not state.ismount.device(uuid=devices[item] .get('uuid')):
-				module.udisksctl('udisksctl mount -b %s' % path.join(module.path.dev, item))
+			label = devices[item] .get('label')
 			
 			
-		return __class__.device()
+			uuid = label is None
+			if not uuid:
+				if state.ismount.device(uuid=devices[item]['uuid']):
+					push(item)
+			else:
+				
+				if state.ismount.device(label=label):
+					push(item)
+			
+			module.udisksctl('udisksctl mount -b %s > /dev/null 2>&1' % path.join(module.path.dev, item))
+		return __class__.__result
 	# def mount(devices):
 	#	pass
 def all(devices): 
