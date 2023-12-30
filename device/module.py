d1fcc5be832a140728b9cd208dd1368205aa4c98
knowhw <142631271+knowhw@users.noreply.github.com>
2023-12-30 17:36:31 +0000

    Update module.py

--- device/module.py
+++ device/module.py
@@ -2,6 +2,7 @@
 from os import popen
 from os import path as mountpoint
 from os import system as udisksctl
+from subprocess import call
 
 from os import getenv
 from json import dumps, loads
@@ -13,14 +14,17 @@ class path:
 	mountpoint=f"/media/{getenv('USER')}"
 	dev="/dev"
 
-
 class collections:
 	dictionary={}
 	array=list
 class keys:
 	NAME,FSTYPE,LABEL,UUID,MOUNTPOINT="NAME","FSTYPE","LABEL","UUID","MOUNTPOINT";
-	
 
+class watch:
+	class state:
+		def devices():
+			for item in call(["dconf", "watch", "/"]):
+				yield item 
 
 
 	
