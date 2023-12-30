bfd45b1392e40a84d78046a66b40d983d8db0c74
knowhw <142631271+knowhw@users.noreply.github.com>
2023-12-30 13:40:10 +0000

    Create mount.py

---
+++ device/mount.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from os import path 
+from device import lsblk
+from device import *
+
+
+class state:
+	@classmethod
+	def device(self):
+		return lsblk.devices()
+		
+	def all(devices):
+		
+		
+		keys = module.collections.array(devices.keys())
+		
+		for item in keys:
+
+			module.udisksctl('udisksctl mount -b %s' % path.join(module.path.dev, item))
+			
+			return __class__.device()
+			
+	# def mount(devices):
+	#	pass
+def all(devices): 
+	""" # device.mount.all(devices) """
+	return state.all(devices)
+	
